163e6d5671574a8bdb22ce3c38eb7453b43df64e
glory.sr <glory.sr@PIA3249>
2022-12-06 12:34:57 +0530

    Add project files.

---
+++ ActionCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ActionCalculator
+{
+    class Program
+    {
+        
+            static void Calculator(Action<int, int> cal)
+            {
+             cal(10, 2);
+            }
+
+            static void Main(string[] args)
+            {
+                Action<int, int> callAdd = new Action<int, int>(Add);
+
+                Action<int, int> callSubtract = new Action<int, int>(Subtract);
+
+                Action<int, int> callProduct = new Action<int, int>(Product);
+
+                Action<int, int> callDivide = new Action<int, int>(Divide);
+
+                Calculator(callAdd+=callSubtract+=callProduct+=callDivide);
+
+
+        }
+
+            static void Add(int firstNumber, int secondNumber)
+            {
+                Console.WriteLine($"Sum is { firstNumber + secondNumber}");
+            }
+
+            static void Subtract(int firstNumber, int secondNumber)
+            {
+            Console.WriteLine($"Difference is { firstNumber - secondNumber}");
+            }
+
+            static void Product(int firstNumber, int secondNumber)
+            {
+            Console.WriteLine($"Product is { firstNumber * secondNumber}");
+            }
+
+             static void Divide(int firstNumber, int secondNumber)
+            {
+            Console.WriteLine($"Divide is { firstNumber / secondNumber}");
+            }
+    }
+}
