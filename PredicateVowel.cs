576e370e89df444242baa76f686f048b4836c3e6
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-06 12:40:34 +0530

    Add files via upload

---
+++ PredicateVowel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PredicateVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<char> vowelCheck = new Predicate<char>(IsVowel);
+
+            VowelFunction(vowelCheck);
+        }
+        static void VowelFunction(Predicate<char> vowel)
+        {
+            Console.WriteLine(vowel('A'));
+            Console.WriteLine(vowel('a'));
+            Console.WriteLine(vowel('T'));
+            Console.WriteLine(vowel('t'));
+            Console.WriteLine(vowel('K'));
+            Console.WriteLine(vowel('k'));
+        }
+        static bool IsVowel(char input)
+        {
+            bool Isvowel = false;
+            switch(char.ToLower(input))
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Isvowel = true;
+                    break;
+                default:
+                    break;
+            }
+            if (Isvowel)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
