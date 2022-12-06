7b1d2e8fb93c9c5991026bc2b175939e66985bb2
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-06 12:36:45 +0530

    Add files via upload

---
+++ FuncCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FuncCalculator
+{
+    class Program
+    {
+
+        static void  Calculator(Func<int,int,int> cal)
+        {
+            Console.WriteLine(cal(10, 2));
+        }
+
+        static void Main(string[] args)
+        {
+           Func<int,int,int> callAdd = new Func<int,int,int>(Add);
+
+           Func<int, int, int> callSubtract= new Func<int, int, int>(Subtract);
+
+           Func<int, int, int> callProduct = new Func<int, int, int>(Product);
+
+           Func<int, int, int> callDivide = new Func<int, int, int>(Divide);
+
+           Calculator(callAdd );
+
+           Calculator(callSubtract);
+
+           Calculator(callProduct);
+
+           Calculator(callDivide);
+        }
+
+        static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        static int Subtract(int firstNumber, int secondNumber)
+        {
+           return firstNumber - secondNumber;
+        }
+
+        static int Product(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        static int Divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+
+
+    }
+}
